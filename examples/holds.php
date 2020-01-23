2f038d9f390c14c01069c8d49c6fd1b7dd5601b1
Paul Rock <paul@drteam.rocks>
2020-01-23 17:23:33 +0300

    additional examples added

---
+++ examples/holds.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'productId'    => '285RNJ9RT141BEAC4082',
+    'eventId'      => '285KX7H3M146B326D284_2015-03-31',
+    'customer'     => [
+        'firstName'    => 'Joe',
+        'lastName'     => 'Smith',
+        'emailAddress' => 'eng@bookeo.com',
+        'phoneNumbers' => [
+            [
+                'number' => '123456',
+                'type'   => 'mobile'
+            ]
+        ]
+    ],
+    'participants' => [
+        'numbers' => [
+            [
+                'peopleCategoryId' => 'Cadults',
+                'number'           => 1
+            ],
+            [
+                'peopleCategoryId' => 'Cchildren',
+                'number'           => 1
+            ]
+        ]
+    ],
+];
